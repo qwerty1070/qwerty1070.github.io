c85b02756243f3e36035e29d3d779167b036d969
qwerty1070 <78442964+qwerty1070@users.noreply.github.com>
2021-03-29 21:31:37 +0500

    добавил папку Projekt
    добавил сайт Гипсолит

---
+++ Projekt/src/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    $('.popup-btn').on('click', function (e) {
+        e.preventDefault();
+        $('.popup').fadeIn(500);
+    });
+
+    $('.popup-close, .popup').on('click', function (e) {
+        e.preventDefault();
+        $('.popup').fadeOut(100);
+    });
+
+    $('.feedback-slider').slick({
+        prevArrow: `<button typr="button" class="feedback-slider-btn feedback-prev-btn"><img src="img/feedback/left-arrow.png"></button>`,
+        nextArrow: `<button typr="button" class="feedback-slider-btn feedback-next-btn"><img src="img/feedback/right-arrow.png"></button>`
+    });
+
+    $('.feature-slayder').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    prevArrow: '<button class="prev arrow"></button>',
+                    nextArrow: '<button class="next arrow"></button>'
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    prevArrow: '<button class="prev arrow"></button>',
+                    nextArrow: '<button class="next arrow"></button>'
+                }
+            }
+        ]
+    });
+
+    new WOW().init();
+
+    $("#phone").mask("+7 (999) 999-99-99");
+});
